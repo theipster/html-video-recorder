f79d00fa13c2c06d68e83a6d882f7edbf5797b54
Alan Ip <theipster@users.noreply.github.com>
2024-10-13 14:07:03 +0100

    🧪 Skip webm fix

--- src/index.js
+++ src/index.js
@@ -1,6 +1,5 @@
 import { concat as concatBlobs } from "./blob.js";
 import { createRecorderFor } from "./recorder.js";
-import fixWebmMetaInfo from "@w-xuefeng/fix-webm-metainfo";
 
 export async function record(media, autoRewind = true) {
   if (autoRewind) {
@@ -12,7 +11,6 @@ export async function record(media, autoRewind = true) {
   media.play();
   return recorder
     .then(concatBlobs)
-    .then(fixWebmMetaInfo)
     .then(URL.createObjectURL);
 }
 
